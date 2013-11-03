c6d1053f3f22cd9ee5a2bd3ca33f40dbf4e58f87
windgazer <martin@windgazer.nl>
2013-11-03 17:04:41 +0100

    grunt file linted, not happy with outcome, but it still works.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,39 +1,57 @@
-module.exports = function(grunt) {
+/* global module, require */
+module.exports = function( grunt ) {
+    "use strict";
 
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    require( "matchdep" ).filterDev( "grunt-*" ).forEach( grunt.loadNpmTasks );
 
     // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-                '<%= grunt.template.today("yyyy-mm-dd") %> */'
+    grunt.initConfig( {
+        pkg : grunt.file.readJSON( "package.json" ),
+        uglify : {
+            options : {
+                banner : "/*! <%= pkg.name %> - v<%= pkg.version %> - " +
+                         "<%= grunt.template.today('yyyy-mm-dd') %> */"
             },
-            my_target: {
-                files: grunt.file.expandMapping( ['*.js','!Gruntfile.js'], '<%= pkg.build.target %>-minified/' )
+            my_target : {
+                files : grunt.file.expandMapping( [ "*.js", "!Gruntfile.js" ],
+                        "<%= pkg.build.target %>-minified/" )
             }
         },
-        copy: {
-            minify: {
-                files: grunt.file.expandMapping(
-                    ['libs/rsvp/browser/rsvp.min.js','package.json']
-                    , '<%= pkg.build.target %>-minified/'
-                    , {
-                        flatten: Boolean,
-                        rename: function(dest, matchedSrcPath, options) {
-                        return dest + matchedSrcPath.replace('.min.js','.js');
-                    } }
+        copy : {
+            minify : {
+                files : grunt.file.expandMapping (
+                    [
+                        "libs/rsvp/browser/rsvp.min.js", "package.json"
+                    ],
+                    "<%= pkg.build.target %>-minified/",
+                    {
+                        flatten : Boolean,
+                        rename : function( dest, matchedSrcPath, options ) {
+                            return dest +
+                                matchedSrcPath.replace( ".min.js", ".js" );
+                        }
+                    }
                 )
-            },
-            normal: {
-                files: grunt.file.expandMapping( ['*.js','!Gruntfile.js','libs/rsvp/browser/rsvp.js'], '<%= pkg.build.target %>-normal/', { flatten: Boolean } )
+                },
+                normal : {
+                    files : grunt.file.expandMapping (
+                        [
+                            "*.js", "!Gruntfile.js",
+                            "libs/rsvp/browser/rsvp.js"
+                        ],
+                        "<%= pkg.build.target %>-normal/",
+                        {
+                            flatten : Boolean
+                        }
+                    )
+                    }
+                }
             }
-        }
-    });
+        )
+    ;
 
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'copy:minify']);
-    grunt.registerTask('npm', ['copy:normal']);
+    grunt.registerTask( "default", [ "uglify", "copy:minify" ] );
+    grunt.registerTask( "npm", [ "copy:normal" ] );
 
 };
